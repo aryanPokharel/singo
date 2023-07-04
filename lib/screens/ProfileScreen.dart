b4702f51d4edfea22a52823600c6de3688aa0914
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-07-05 02:40:37 +0545

    profiles page created and made dynamic

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -12,16 +12,71 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    var userName = context.watch<UserProvider>().user;
+    var myUser = context.watch<UserProvider>().user;
     return Scaffold(
-      body: Center(
+      backgroundColor: const Color.fromARGB(255, 203, 200, 200),
+      body: SingleChildScrollView(
         child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                print(userName.toJson());
-              },
-              child: const Text("Press"),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 10,
+            ),
+            const CircleAvatar(
+              radius: 80,
+              backgroundImage: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              myUser.fullName.toString(),
+              style: const TextStyle(
+                fontFamily: 'Sacramento',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            const Text(
+              "Some Subtitle : ",
+              style: TextStyle(fontSize: 15, fontFamily: 'EBGaramond'),
+            ),
+            const SizedBox(
+              height: 10,
+              width: 150,
+              child: Divider(
+                thickness: 1,
+                color: Colors.black,
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              color: const Color.fromARGB(255, 225, 215, 215),
+              child: ListTile(
+                leading: const Icon(Icons.phone),
+                title: Text(
+                    '+${myUser.phone['countryCode'].toString()} ${myUser.phone['number'].toString()}'),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              color: const Color.fromARGB(255, 240, 223, 223),
+              child: ListTile(
+                leading: const Icon(Icons.mail),
+                title: Text(myUser.email.toString()),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              color: const Color.fromARGB(255, 210, 178, 178),
+              child: ListTile(
+                leading: const Icon(Icons.cake),
+                title: Text(myUser.dob.toString()),
+              ),
             )
           ],
         ),
